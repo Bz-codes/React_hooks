dc4aa7fe3482594afd2315a51117e18acc03e9b9
Bz-codes <obijimarvelous25@gmail.com>
2023-07-25 16:47:38 +0100

    updated movie app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import MovieList from "./components/MovieList";
+import Filter from "./components/Filter";
+import AddMovie from "./components/AddMovie";
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([
+    // Initial movie data
+    {
+      title: "Movie 1",
+      description: "Description of Movie 1",
+      posterURL: "./logo192.png",
+      rating: 7.5,
+    },
+    // Add more movie objects here as needed
+  ]);
+
+  const handleFilter = ({ title, rating }) => {
+    // Filter the movies based on title and rating
+    // You can implement the logic to filter the movies here
+    // For simplicity, we will just filter the title for this example
+    const filteredMovies = movies.filter((movie) =>
+      movie.title.toLowerCase().includes(title.toLowerCase())
+    );
+    setMovies(filteredMovies);
+  };
+
+  const handleAddMovie = (newMovie) => {
+    // Add a new movie to the list
+    setMovies([...movies, newMovie]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Movie App</h1>
+      <Filter onFilter={handleFilter} />
+      <MovieList movies={movies} />
+      <AddMovie handleAddMovie={handleAddMovie} />
     </div>
   );
-}
+};
 
 export default App;
